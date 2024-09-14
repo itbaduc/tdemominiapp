515ee77a7c541f2ed25afe29911da0ad63eb572d
DucNB <itbaduc@gmail.com>
2024-09-13 20:11:34 -0700

    new build

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
+    outDir: "./build",
+    assetsDir: "./assets",
   },
 });
